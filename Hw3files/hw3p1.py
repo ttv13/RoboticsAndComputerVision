55a06806edbc734dc4523789f89837c751e5d7ed
unknown <v.u.t.thomas@gmail.com>
2024-10-21 13:14:37 -0400

    completed hw3p1

--- Hw3files/hw3p1.py
+++ Hw3files/hw3p1.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import glob
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 # Termination criteria for cornerSubPix function for corner refinement 
 criteria = (cv2.TermCriteria_EPS + cv2.TermCriteria_MAX_ITER, 30, 0.001)
@@ -45,8 +46,8 @@ for fname in images:
 
 
         img = cv2.drawChessboardCorners(resizeImg, checkerboard_size, corners2, ret)
-        cv2.imshow('img', img)
-        cv2.waitKey(1000)
+        cv2.imshow("img", img)
+        cv2.waitKey(100)
     else:
         print(f"No corners found in {fname}")
 
@@ -57,27 +58,44 @@ cv2.destroyAllWindows()
 ret, K, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 
-print("Camera matrix (K):\n", K)
-# print("\nR matrices and t vectors for 3 of the camera views:\n")
+print("Intrinsic matrix (K):\n", K)
+print ("r and t ", rvecs[1])
+print("\nR matrices and t vectors for 3 of the camera views:\n")
 
-# for i in range(3):
-#     print(f"View {i+1}:")
-#     R, _ = cv2.Rodrigues(rvecs[i])
-#     print("R:\n", R)
-#     print("t:\n", tvecs[i])
-#     print()
+for i in range(3):
+    print(f"View {i+1}:")
+    #rotation vector to rotation matrix
+    R, _ = cv2.Rodrigues(rvecs[i])
 
-# # Plot the camera locations
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
+    print("R:\n", R)
+    print("t:\n", tvecs[i],"\n")
 
-# for i in range(len(rvecs)):
-#     R, _ = cv2.Rodrigues(rvecs[i])
-#     camera_location = -np.matrix(R).T * np.matrix(tvecs[i])
-#     ax.scatter(camera_location[0], camera_location[1], camera_location[2], c='b', marker='o')
+# Plot the camera locations
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-# ax.set_xlabel('X')
-# ax.set_ylabel('Y')
-# ax.set_zlabel('Z')
-# plt.title('Camera Locations in World Coordinates')
-# plt.show()
+#plotting checkerboard
+Cbplot = np.array([[0,0,0], [checkerboard_size[0],0,0], [checkerboard_size[0],checkerboard_size[1],0], [0,checkerboard_size[1],0]])
+ax.plot(Cbplot[:, 0], Cbplot[:, 1], Cbplot[:, 2], 'r-')
+
+for i in range(len(rvecs)):
+    #rotation vector to rotation matrix
+    R, _ = cv2.Rodrigues(rvecs[i])
+
+    #camera location (with respect to world coordinate system)
+    wtc = -np.matrix(R).T * np.matrix(tvecs[i])
+    wtc = np.array(wtc).flatten()
+    print(wtc,"\n")
+    ax.scatter(wtc[0], wtc[1], -wtc[2], c='b', marker='o')
+    ax.text(wtc[0], wtc[1], -wtc[2], f'{i+1}', size=10, zorder=1, color='k')  # Adding labels
+
+    #camera orientation
+    camera_direction = R.T @ np.array([0, 0, 1]).reshape(-1, 1)
+    camera_direction = camera_direction.flatten()
+    ax.quiver(wtc[0], wtc[1], -wtc[2], camera_direction[0], camera_direction[1], -camera_direction[2], color='r')
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+plt.title('Camera Locations in World Coordinates')
+plt.show()
